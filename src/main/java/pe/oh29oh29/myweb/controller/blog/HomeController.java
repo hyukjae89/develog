327f987d68ef8ad925797203591c36126836f219
Hyukjae <hyukjae89@gmail.com>
2018-06-25 18:36:36 +0900

    js, css minify 관련 수정

--- src/main/java/pe/oh29oh29/myweb/controller/blog/HomeController.java
+++ src/main/java/pe/oh29oh29/myweb/controller/blog/HomeController.java
@@ -1,24 +1,16 @@
 package pe.oh29oh29.myweb.controller.blog;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import pe.oh29oh29.myweb.common.config.Dir;
-import pe.oh29oh29.myweb.service.PostService;
 
 @Controller
 public class HomeController {
 	
-	@Autowired PostService postService;
-	@Autowired Dir dir;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		model.addAttribute("jsDir", dir.getJs());
-		model.addAttribute("cssDir", dir.getCss());
 		return "blog/home";
 	}
 }
